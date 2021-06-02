58fd3ffa64d422e04bf44786b4761f9df2f7a305
Bara Salameh <barasalameh90@gmail.com>
2021-06-02 17:33:54 +0300

    Push project

---
+++ products_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from users_app.models import User
+
+# Create your models here.
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    category_id = models.ForeignKey(ProductCategory, related_name='category_products', on_delete=CASCADE)
+    order = models.ManyToManyField(User, related_name='user_products')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ users_app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    building_number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    logo = models.CharField(max_length=255, default="https://www.pngkey.com/png/detail/62-627900_white-question-mark-on-a-black-circular-background.png")
+    category_id = models.ForeignKey(UserCategory, related_name='wholesalers', on_delete=CASCADE, null=True)
+    address_id = models.OneToOneField(Address, on_delete=CASCADE)
+    role_id = models.ForeignKey(Role, related_name= 'type', on_delete=CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
