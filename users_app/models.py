fe846381e8f32f4d03e7cb5eccf9506e05a55555
jad dawani <dawanijad@gmail.com>
2021-06-05 14:21:05 +0300

    validation

--- users_app/models.py
+++ users_app/models.py
@@ -1,6 +1,48 @@
+from typing import Optional
 from django.db import models
 from django.db.models.deletion import CASCADE
+import bcrypt
+import re
 
+class BlogManager(models.Manager):
+    def basic_validator(self, postData ):
+        errors = {}
+        LOGO_REGEX = re.compile(r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)", re.MULTILINE|re.UNICODE)
+        if not LOGO_REGEX.match(postData['logo']):
+            errors['logo'] = "Invalid logo path!"
+
+
+        EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
+        if not EMAIL_REGEX.match(postData['Email']):    
+            errors['Email'] = "Invalid email address!"
+        if len(postData['client_name']) < 2:
+            errors["client_name"] = "client name should be at least 2 characters"
+        if len(postData['phone_number']) < 7:
+            errors["phone_number"] = "phone number should be at least 7 characters"
+        if len(postData['passwd']) < 8:
+            errors["passwd"] = "your password should be at least 8 characters"
+        if len(postData['city']) < 2:
+            errors["city"] = "your city should be at least 2 characters"
+        if len(postData['street']) < 2:
+            errors["street"] = "your city should be at least 2 characters"
+        if len(postData['building_number']) < 1:
+            errors["building_number"] = "your building_number should be at least 1 characters"
+        
+
+       
+        return errors
+
+        
+    def validate_login(self, email , password):
+        errors = {}
+        EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
+        if not EMAIL_REGEX.match(email['Email']):    
+            errors['Email'] = "Invalid email address!"
+        if len(password['password']) < 8:
+            errors["password"] = "your password should be at least 8 characters"
+        return errors
+        
+    
 
 class Role(models.Model):
     name = models.CharField(max_length=255)
