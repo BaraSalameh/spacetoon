09670a0d71d017281edcc4f62b677d8980d8edc2
Yousef-Nour <yousefnour809@gmail.com>
2021-06-05 15:49:21 +0300

    procurment logo rm bg

--- users_app/models.py
+++ users_app/models.py
@@ -66,13 +66,10 @@ class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-<<<<<<< HEAD
-=======
     logo = models.CharField(max_length=255, default="https://www.pngkey.com/png/detail/62-627900_white-question-mark-on-a-black-circular-background.png")
     phone_number=models.CharField(max_length=255, default=True)
     category_id = models.ForeignKey(UserCategory, related_name='wholesalers', on_delete=CASCADE, null=True)
     address_id = models.OneToOneField(Address, on_delete=CASCADE)
->>>>>>> e51cec675657e661c5aab2efbfabdeeac17be158
     role_id = models.ForeignKey(Role, related_name= 'type', on_delete=CASCADE)
     address_id = models.OneToOneField(Address, on_delete=CASCADE)
     category_id = models.ForeignKey(UserCategory, related_name='wholesalers', on_delete=CASCADE, null=True)
